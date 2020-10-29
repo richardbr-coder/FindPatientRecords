5837615151f194fe6d6bccf5bc1a08f8e2083382
Richard <52008509+richardbr-coder@users.noreply.github.com>
2020-10-29 14:45:54 -0230

    Add files via upload

---
+++ FindPatientRecords/FindPatientRecords/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace FindPatientRecords
+{
+    //The main class
+    class Program
+    {
+        //The main
+        static void Main(string[] args)
+        {
+            // Read and show each line from the file.
+            string line = "";
+            int id;
+            Console.WriteLine("Enter ID to be searched:");
+            id = Convert.ToInt32(Console.ReadLine());
+            //Try
+            try
+            {
+                //text reader
+                using (StreamReader sr = new StreamReader("PatientRecords.txt"))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        //split the line and extract the id
+                        string[] tokens = line.Split(' ');
+                        //if id's are the same
+                        if (Convert.ToInt32(tokens[0]) == id)
+                            Console.WriteLine(line);
+                    }
+                }
+            }
+            //Exception handling
+            catch
+            {
+                Console.WriteLine("ERROR - The file is invalid");
+            }
+            Console.ReadKey();
+        }
+    }
+}
